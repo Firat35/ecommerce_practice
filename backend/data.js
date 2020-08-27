796ec9a3b0f71d30a7e4c39cf3e05f4f5ae922a0
Firat <fratpalta@gmail.com>
2020-08-27 15:55:05 +0300

    first commit

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+export default  {
+    products:[
+        {   
+            _id : "1",
+            name : "Blue Shirt",
+            category : "Shirts",
+            image : "/images/shirt-1.png",
+            price : 60,
+            brand : "Adidas",
+            rating : 4.5,
+            numReviews : 10,
+            countInStock: 0
+        },
+        {
+            _id : "2",
+            name : "Blue Sweatsuit",
+            category : "Pants",
+            image : "/images/pant-1.png",
+            price : 65,
+            brand : "Nike",
+            rating : 3.8,
+            numReviews : 11,
+            countInStock: 7
+        },
+        {
+            _id : "3",
+            name : "Black Sweatsuit",
+            category : "Pants",
+            image : "/images/pant-2.png",
+            price : 70,
+            brand : "Adidas",
+            rating : 4.7,
+            numReviews : 15,
+            countInStock: 8
+        },        
+        {
+            _id : "4",
+            name : "Blue Pant",
+            category : "Pants",
+            image : "/images/pant-3.png",
+            price : 90,
+            brand : "Zara",
+            rating : 5,
+            numReviews : 90,
+            countInStock: 9
+        },
+        {
+            _id : "5",
+            name : "Red Shirt",
+            category : "Shirts",
+            image : "/images/shirt-2.png",
+            price : 100,
+            brand : "Polo",
+            rating : 4.8,
+            numReviews : 7,
+            countInStock: 3
+        },
+        {
+            _id : "6",
+            name : "Black Shirt",
+            category : "Shirts",
+            image : "/images/shirt-3.png",
+            price : 40,
+            brand : "Zara",
+            rating : 4.3,
+            numReviews : 5,
+            countInStock: 1
+        }
+    ]
+}
